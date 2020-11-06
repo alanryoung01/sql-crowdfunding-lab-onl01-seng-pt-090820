43bc1ef28e06c7cc857ae8111a37b5a4b7d648c6
Alan Young <alanryoung01@gmail.com>
2020-11-06 10:08:07 +0000

    Automatically backed up by Learn

--- lib/create.sql
+++ lib/create.sql
@@ -7,3 +7,18 @@ CREATE TABLE projects (
   end_date TEXT
 )
 ;
+
+CREATE TABLE users (
+id INTEGER PRIMARY KEY,
+name TEXT,
+age INTEGER
+)
+;
+
+CREATE TABLE pledges (
+id INTEGER PRIMARY KEY,
+amount NUMERIC,
+user_id INTEGER,
+project_id INTEGER
+)
+;
